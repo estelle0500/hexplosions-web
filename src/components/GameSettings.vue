6afdb268ad117df4031fc1a858981e72d28df987
estelle0500 <estelle0500@gmail.com>
2020-04-05 02:40:40 +0800

    Add component to handle settings selection

---
+++ src/components/GameSettings.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>
+            <h2> Select number of players: </h2>
+            <input type="range" min="2" max="4" v-model="numPlayers">
+        </div>
+        <div>
+            <h2> Select grid size: </h2>
+            <div v-for="sizeName in Object.keys(gameGridSizes)" v-bind:key="sizeName">
+                <input type="radio" v-model="gridSize" v-bind:value="gameGridSizes[sizeName]" v-bind:id="sizeName">
+                {{ size }}
+                <label v-bind:for="sizeName"> {{ sizeName }} </label>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class GameSettings extends Vue {
+    gameGridSizes: any = {
+      SMALL: 5,
+      MEDIUM: 7,
+      LARGE: 9
+    }
+    numPlayers: number = 2
+    gridSize: number = this.gameGridSizes.SMALL
+}
+</script>
