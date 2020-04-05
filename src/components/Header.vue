2c9fcb387bda8064e58b1fd8167c7f1e2df998ba
estelle0500 <estelle0500@gmail.com>
2020-04-06 04:27:43 +0800

    Add header to display game title
    TODO: Link header to landing page

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <div id="linked-title">
+            HEXPLOSIONS!
+        </div>
+    </header>
+</template>
+
+<style scoped>
+@font-face {
+    font-family: Retro;
+    src: url('../assets/reblinking.ttf');
+}
+
+#linked-title {
+    font-family: Retro;
+    font-size: 3em;
+    color: white;
+}
+</style>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Header extends Vue {}
+</script>
